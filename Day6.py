14840ead65d5b5425dfe027e22a16941985907ed
Manish Singh <161852461+manishs21@users.noreply.github.com>
2025-09-13 21:14:13 +0530

    Updated Day6.py

--- Day6.py
+++ Day6.py
@@ -1,3 +1,37 @@
+  #Looping or iterative statements
+
+  # for loop
+fruits = ["apple","Banana","Kiwi","Mango","Watermelon"]
+for fruit in fruits:
+      print(fruit)
+
+my_list = list(range(1, 11))
+print(my_list)
+
+#  i need to print the numbers from 1 to 10
+for i in range(1,21,2):
+     print(i,end=" ")
+my_dic={
+     "1": "Apple",
+     "2":"Banana",
+     "3":"mango"}
+for k in my_dic:
+   print(f"{k}: {my_dic[k]}")
+
+fruits = ["Apple","Mango","Banana"]
+count = 0
+for fruit in fruits:
+     count = count+1
+     print(f"{count} : {fruit}")
+
+#infinite loop
+
+count=0
+while count<=10:
+     print("My name is Manish ")
+     count+=1
+
+
 for i in range(1,21,2):
     print(i, end = " ")
 
@@ -5,3 +39,4 @@ print()
 
 for j in range(0,21,2):
     print(j, end = " ")
+    
